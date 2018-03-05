817d0d186d2f282d38f1f0f814c20a8a7ce14608
Keely Brennan <keelybrennan@Keelys-MacBook-Pro-2.local>
2018-03-04 19:20:26 -0600

    pseudo code and initial html and bootstrap containers

---
+++ assets/javascript/game.js
@@ -0,0 +1,16 @@
+//1. 4 crystals should display on the page each with a differnet random value between 1 and 12. 
+//2. As the user clicks on the crystals, userScore us incremented until they reach the numberToGuess.
+
+//3. numberToGuess is randomly generated between 19 and 120 and displayed on the page.
+
+//4. userScore is compared to numberToGuess. 
+    // IF the userScore is less than the numberToGuess, the user may continue to click the crystals to add points to the userScore
+        // ELSE - when userScore == numberToGuess, 
+            //the round is over
+            // the wins are incrememted by 1 
+            // game resets so the user can play another round 
+        // ELSE IF - when userScore > numberToGuess, the user loses the round. 
+            //the round is over
+            // the wins are incrememted by 1 
+            // game resets so the user can play another round 
+
